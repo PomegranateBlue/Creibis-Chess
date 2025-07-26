e1a69c9ba4c555108228fedbc1ce0a91a05de7a1
bluepome98 <wpdn1998@gmail.com>
2025-07-26 16:41:38 +0900

    style : flex-자연수 형태로 사용할 것, tailwind Intellisence의 잘못된 자동완성 기능으로 flex-1/2 등 표시되는 중

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import { Chessboard } from "react-chessboard";
 
 const App = () => {
   return (
-    <main className="flex items-center flex-row justify-center overflow-hidden h-screen w-screen ">
+    <main className="flex flex-1/3 items-center flex-row justify-center overflow-hidden h-screen w-screen ">
       <section>
-        <h1 className="flex border-white border tracking-widest font-extrabold italic text-[48px] [text-shadow:0_2px_0_white,2px_0_0_white,0_-2px_0_white,-2px_0_0_white] text-black">
+        <h1 className="flex flex-1/2 border-white border tracking-widest font-extrabold italic text-[48px] [text-shadow:0_2px_0_white,2px_0_0_white,0_-2px_0_white,-2px_0_0_white] text-black">
           Creibis Chess
         </h1>
       </section>
-      <section className="w-full aspect-square max-w-[640px]    max-h-[640px]">
+      <section className="flex flex-1/3 w-full aspect-square max-w-[640px]    max-h-[640px]">
         <Chessboard />
       </section>
-      <section>Another Info</section>
+      <section className="flex flex-1/3">Another Info</section>
     </main>
   );
 };
