84b73168be6c31d8d9360fc5f412c4aa2839d1e0
bluepome98 <wpdn1998@gmail.com>
2025-07-26 16:36:08 +0900

    style : 레이아웃 재배치 중

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { Chessboard } from "react-chessboard";
+
 const App = () => {
   return (
-    <div>
-      <div>
-        <h1 className="tracking-widest font-extrabold italic text-[48px] [text-shadow:0_2px_0_white,2px_0_0_white,0_-2px_0_white,-2px_0_0_white] text-black">
+    <main className="flex items-center flex-row justify-center overflow-hidden h-screen w-screen ">
+      <section>
+        <h1 className="flex border-white border tracking-widest font-extrabold italic text-[48px] [text-shadow:0_2px_0_white,2px_0_0_white,0_-2px_0_white,-2px_0_0_white] text-black">
           Creibis Chess
         </h1>
-      </div>
-    </div>
+      </section>
+      <section className="w-full aspect-square max-w-[640px]    max-h-[640px]">
+        <Chessboard />
+      </section>
+      <section>Another Info</section>
+    </main>
   );
 };
 export default App;
