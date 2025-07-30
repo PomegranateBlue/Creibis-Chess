dddbd1e57a2495bcbbe5ab3fd53aea17ed963be5
bluepome98 <wpdn1998@gmail.com>
2025-07-30 15:47:20 +0900

    feat: 체스보드 옵션 정의, chess.js를 통한 게임 규칙 설정, CPU가 랜덤 수를 두도록 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,57 @@
-import { Chessboard } from "react-chessboard";
-import { useRef } from "react";
+import { Chessboard, type PieceDropHandlerArgs } from "react-chessboard";
+import { useRef, useState } from "react";
 import { Chess } from "chess.js";
 const App = () => {
   const chessGameRef = useRef(new Chess());
-
   const chessGame = chessGameRef.current;
+
+  const [chessPosition, setChessPosition] = useState(chessGame.fen());
+  /**
+   * fen= Forsyth-Edwards Notation은 체스 기물 배치 상태와 현재 게임 상태를 문자열로
+   * 압축해서 나타내는 포맷
+   * 6개의 필드로 구성
+   *
+   */
+  const makeRandomMoves = () => {
+    const possibleMoves = chessGame.moves();
+
+    if (chessGame.isGameOver()) {
+      return;
+    }
+
+    const randomMoves =
+      possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+    chessGame.move(randomMoves);
+    setChessPosition(chessGame.fen());
+  };
+
+  const onPieceDrop = ({
+    sourceSquare,
+    targetSquare,
+  }: PieceDropHandlerArgs) => {
+    if (!targetSquare) {
+      return false;
+    }
+
+    try {
+      chessGame.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: "q", // 항상 queen으로 지정해서 승급할 것,나중에 q,r,k,b에서 선택할 수 있도록하기
+      });
+      setChessPosition(chessGame.fen());
+      setTimeout(makeRandomMoves, 500);
+      return true;
+    } catch {
+      return false;
+    }
+  };
+
+  const chessboardOptions = {
+    position: chessPosition,
+    onPieceDrop,
+    id: "play-vs-random",
+  };
   return (
     <main className="flex items-center flex-row justify-center overflow-hidden h-screen w-screen ">
       <section className="flex flex-1 items-center justify-center">
@@ -14,7 +61,7 @@ const App = () => {
       </section>
       <section className="flex flex-1 items-center justify-center ">
         <div className="w-full max-w-[640px] aspect-square">
-          <Chessboard />
+          <Chessboard options={chessboardOptions} />
         </div>
       </section>
       <section className="flex flex-1 items-center justify-center">
